19012c8d6196a5fe33358963cdbe1578549dc7f9
samirangharami <samirangharami05@gmail.com>
2025-12-03 11:33:32 +0530

    solved part 1 of tyranny rocket problem

---
+++ 2019/tyranny_rocket_equation/tyranny_rocket.js
@@ -0,0 +1,17 @@
+const feulRequired = (mass) => {
+  mass / 3 - 2;
+};
+
+const sum = (x, y) => x + y;
+
+export const totalFeulRequired = (masses) => {
+  const massesInString = masses;
+  const feulRequiredForEach = massesInString
+    .split("\n")
+    .map((mass) => Math.floor(feulRequired(mass)))
+    .reduce((totalFeul, feul) => sum(totalFeul, feul), 0);
+
+  return feulRequiredForEach;
+};
+
+console.log(totalFeulRequired(Deno.readTextFileSync("../input.txt")))
--- README.md
+++
@@ -1,2 +0,0 @@
-# advent_of_code
-place to solve advent of code problems
