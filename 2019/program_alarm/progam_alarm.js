230041c1c249ea6634905357df217e5a7a32955b
samirangharami <samirangharami05@gmail.com>
2025-12-03 14:43:18 +0530

    solved part 2 of program alarm problem

--- 2019/program_alarm/progam_alarm.js
+++ 2019/program_alarm/progam_alarm.js
@@ -4,16 +4,16 @@ const add = (num1, num2) => num1 + num2;
 
 const mul = (num1, num2) => num1 * num2;
 
-const programAlarm = (program) => {
+const programAlarm = (program, address1, address2) => {
   const codes = program.split(",").map((code) => parseInt(code));
-  codes[1] = 12;
-  codes[2] = 2;
+  codes[1] = address1;
+  codes[2] = address2;
   for (let index = 0; index < codes.length; index += 4) {
     const opCode = codes[index];
     const num1 = codes[codes[index + 1]];
     const num2 = codes[codes[index + 2]];
     const storageIndex = codes[index + 3];
-    
+
     if (opCode === 1) codes[storageIndex] = performOperation(add, num1, num2);
     if (opCode === 2) codes[storageIndex] = performOperation(mul, num1, num2);
     if (opCode === 99) break;
@@ -22,4 +22,12 @@ const programAlarm = (program) => {
   return codes[0];
 };
 
-console.log(programAlarm(Deno.readTextFileSync("input.txt")));
+const processedAddress = (program, idealOutput) => {
+  for (let i = 0; i < 100; i++) {
+    for (let j = 0; j < 100; j++) {
+      if (programAlarm(program, i, j) === idealOutput) return 100 * i + j;
+    }
+  }
+};
+
+console.log(processedAddress(Deno.readTextFileSync("input.txt"), 19690720));
