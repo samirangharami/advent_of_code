fbd0c87eb00ee179be20b6839912af7a28f86a36
samirangharami <samirangharami05@gmail.com>
2025-12-03 11:54:45 +0530

    solved part of tyranny rocket problem

--- 2019/tyranny_rocket_equation/tyranny_rocket.js
+++ 2019/tyranny_rocket_equation/tyranny_rocket.js
@@ -1,15 +1,24 @@
-const feulRequired = (mass) => mass / 3 - 2;
+const feulRequired = (mass, type = "feul") => {
+  const massOfObject = parseInt(mass);
+  if (massOfObject < 0) return 0;
+  const massOfFeul = massOfObject / 3 - 2;
+  return type === "feul"
+    ? (massOfObject + feulRequired(massOfFeul))
+    : massOfFeul;
+};
 
 const sum = (x, y) => x + y;
 
-export const totalFeulRequired = (masses) => {
+const totalFeulRequired = (masses) => {
   const massesInString = masses;
   const feulRequiredForEach = massesInString
     .split("\n")
-    .map((mass) => Math.floor(feulRequired(mass)))
+    .map((rocktMass) =>
+      Math.floor(feulRequired(feulRequired(rocktMass, "rocket")))
+    )
     .reduce((totalFeul, feul) => sum(totalFeul, feul), 0);
 
   return feulRequiredForEach;
 };
 
-console.log(totalFeulRequired(Deno.readTextFileSync("../input.txt")))
+console.log(totalFeulRequired(Deno.readTextFileSync("input.txt")));
