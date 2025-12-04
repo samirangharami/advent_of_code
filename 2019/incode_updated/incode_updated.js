17e224301383abda66fec55738af3b4fa5e66348
samirangharami <samirangharami05@gmail.com>
2025-12-05 01:37:18 +0530

    solved day 5 part 1

--- 2019/incode_updated/incode_updated.js
+++ 2019/incode_updated/incode_updated.js
@@ -1,25 +1,48 @@
+const operations = { 1: add, 2: mul };
+const add = (num1, num2) => num1 + num2;
+const mul = (num1, num2) => num1 * num2;
 const performOperation = (operation, num1, num2) => operation(num1, num2);
 
-const add = (num1, num2) => num1 + num2;
+const performOpCode1And2 = (
+  codes,
+  opCode,
+  parameter1,
+  parameter2,
+  parameter3,
+) => {
+  const fullOpCode = (opCode.toString()).padStart(5, "0");
+  const num2 = (fullOpCode[1] === "0") ? codes[parameter2] : parameter2;
+  const num1 = (fullOpCode[2] === "0") ? codes[parameter1] : parameter1;
 
-const mul = (num1, num2) => num1 * num2;
+  codes[parameter3] = performOperation(
+    operations[fullOpCode.at(-1)],
+    num1,
+    num2,
+  );
+};
 
 const programAlarm = (program) => {
   const codes = program.split(",").map((code) => parseInt(code));
-  codes[1] = 12;
-  codes[2] = 2;
-  for (let index = 0; index < codes.length; index += 4) {
+
+  for (let index = 0; index < codes.length; index += 2) {
     const opCode = codes[index];
-    const num1 = codes[codes[index + 1]];
-    const num2 = codes[codes[index + 2]];
-    const storageIndex = codes[index + 3];
-    
-    if (opCode === 1) codes[storageIndex] = performOperation(add, num1, num2);
-    if (opCode === 2) codes[storageIndex] = performOperation(mul, num1, num2);
+    const parameter1 = codes[index + 1];
+    const parameter2 = codes[index + 2];
+    const parameter3 = codes[index + 3];
+    const addressIndexForInputAndOutput = codes[index + 1];
+
+    if ((opCode % 10 === 1) || (opCode % 10 === 2)) {
+      performOpCode1And2(codes, opCode, parameter1, parameter2, parameter3);
+      index += 2;
+    }
+
+    if (opCode === 3) {
+      codes[addressIndexForInputAndOutput] = parseInt(prompt("enter data"));
+    }
+
+    if (opCode === 4) console.log(codes[addressIndexForInputAndOutput]);
     if (opCode === 99) break;
   }
-
-  return codes[0];
 };
 
-console.log(programAlarm(Deno.readTextFileSync("input.txt")));
+programAlarm(Deno.readTextFileSync("input.txt"));
