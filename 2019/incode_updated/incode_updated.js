d03679e55dbaff7c6ef033366334388a016399d6
samirangharami <samirangharami05@gmail.com>
2025-12-05 10:46:54 +0530

    solved day 5 part 2

--- 2019/incode_updated/incode_updated.js
+++ 2019/incode_updated/incode_updated.js
@@ -1,6 +1,6 @@
-const operations = { 1: add, 2: mul };
 const add = (num1, num2) => num1 + num2;
 const mul = (num1, num2) => num1 * num2;
+const operations = { 1: add, 2: mul };
 const performOperation = (operation, num1, num2) => operation(num1, num2);
 
 const performOpCode1And2 = (
@@ -21,6 +21,39 @@ const performOpCode1And2 = (
   );
 };
 
+const performOpCode5And6 = (codes, opCode, parameter1, parameter2, index) => {
+  const fullOpCode = opCode.toString().padStart(4, "0");
+
+  const addressToJump = (fullOpCode[0] === "0")
+    ? codes[parameter2]
+    : parameter2;
+  const numtoCheck = (fullOpCode[1] === "0") ? codes[parameter1] : parameter1;
+
+  if (fullOpCode.at(-1) === "5") {
+    return numtoCheck !== 0 ? addressToJump - 2 : index + 1;
+  }
+
+  if (fullOpCode.at(-1) === "6") {
+    return numtoCheck === 0 ? addressToJump - 2 : index + 1;
+  }
+};
+
+const performOpCode7And8 = (
+  codes,
+  opCode,
+  parameter1,
+  parameter2,
+  parameter3,
+) => {
+  const fullOpCode = (opCode.toString()).padStart(5, "0");
+
+  const num2 = (fullOpCode[1] === "0") ? codes[parameter2] : parameter2;
+  const num1 = (fullOpCode[2] === "0") ? codes[parameter1] : parameter1;
+
+  if (fullOpCode.at(-1) === "7") codes[parameter3] = num1 < num2 ? 1 : 0;
+  if (fullOpCode.at(-1) === "8") codes[parameter3] = num1 === num2 ? 1 : 0;
+};
+
 const programAlarm = (program) => {
   const codes = program.split(",").map((code) => parseInt(code));
 
@@ -29,18 +62,21 @@ const programAlarm = (program) => {
     const parameter1 = codes[index + 1];
     const parameter2 = codes[index + 2];
     const parameter3 = codes[index + 3];
-    const addressIndexForInputAndOutput = codes[index + 1];
 
     if ((opCode % 10 === 1) || (opCode % 10 === 2)) {
       performOpCode1And2(codes, opCode, parameter1, parameter2, parameter3);
       index += 2;
     }
-
-    if (opCode === 3) {
-      codes[addressIndexForInputAndOutput] = parseInt(prompt("enter data"));
+    if ((opCode % 10 === 5) || (opCode % 10 === 6)) {
+      index = performOpCode5And6(codes, opCode, parameter1, parameter2, index);
+    }
+    if ((opCode % 10 === 7) || (opCode % 10 === 8)) {
+      performOpCode7And8(codes, opCode, parameter1, parameter2, parameter3);
+      index += 2;
     }
 
-    if (opCode === 4) console.log(codes[addressIndexForInputAndOutput]);
+    if (opCode === 4) console.log(codes[parameter1]);
+    if (opCode === 3) codes[parameter1] = parseInt(prompt("enter data"));
     if (opCode === 99) break;
   }
 };
